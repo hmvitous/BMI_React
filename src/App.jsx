93fa32616ae340412a5223421fae5f787e908cad
Hunter <hmvitous@yahoo.com>
2020-02-24 13:15:05 +0100

    Creates Form.jsx, cleans up App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,25 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import Form from "./components/Form";
 
 class App extends Component {
-  render () {
+  state = {
+    weight: "",
+    height: ""
+  };
+
+  onChangeHandler = e => this.setState({ [e.target.name]: e.target.value });
+
+  render() {
     return (
-      <form>
-        <label htmlFor="weight">Weight</label>
-        <input
-          type="number"
-          required
-          placeholder="Weight in kg"
-          name="weight"
-          id="weight"
+      <div>
+        <Form
+          weight={this.state.weight}
+          height={this.state.height}
+          onChangeHandler={this.onChangeHandler}
         />
-         <label htmlFor="height">Height</label>
-          <input
-            type="number"
-            required
-            placeholder="Height in cm"
-            name="height"
-            id="height"
-          />
-          <button>Calculate BMI</button>
-      </form>
+      </div>
     );
   }
 }
-
-
 
 export default App;
